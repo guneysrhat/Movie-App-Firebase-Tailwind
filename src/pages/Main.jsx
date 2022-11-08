93297ed3c34ccaa3ba9c005521445e31249f8ee0
guneysrhat <guneysrhat@gmail.com>
2022-11-08 21:42:14 +0300

    movie card

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
 const Main = () => {
-  return <div>Main</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    axios
+      .get(API)
+      .then((res) => setMovies(res.data.result))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="flex justify-center flex-wrap">
+      {movies.map((movie) => {
+        <MovieCard key={movie.id} />;
+      })}
+    </div>
+  );
 };
 
 export default Main;
